e2033c4b8091299b2afc6f0934b3dd966b58a6b9
montio5 <moein@greenhabitat.at>
2024-07-25 01:04:27 +0330

    refactor: add toast

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -1,15 +1,14 @@
-// LoginScreen.js
-
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Pressable, Alert } from 'react-native';
-import apiUrl from '../../utils/apiConfig';
-import { strings } from '../../utils/strings';
-import {login} from '../../api/Authentication'
-import CustomButton from '../../general/customButtonComponent'
+import React, { useState, useRef } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Pressable } from 'react-native';
+import { login } from '../../api/Authentication'; // Ensure this path is correct
+import { strings } from '../../utils/strings'; // Ensure this path is correct
+import CustomButton from '../../general/customButtonComponent'; // Ensure this path is correct
+import Toast from '../../general/Toast';  // Ensure this path is correct
 
 const LoginScreen = ({ navigation, setIsLoggedIn }) => {
   const [email, setEmail] = useState('mont@gmail.com');
   const [password, setPassword] = useState('1234test');
+  const toastRef = useRef();
 
   const handleLogin = async () => {
     const result = await login(email, password);
@@ -18,7 +17,7 @@ const LoginScreen = ({ navigation, setIsLoggedIn }) => {
       navigation.navigate('Home');
       setIsLoggedIn(true);
     } else {
-      Alert.alert(strings.login.alertTitle, result.message);
+      toastRef.current.error(result.message || 'Error message');
     }
   };
 
@@ -39,15 +38,16 @@ const LoginScreen = ({ navigation, setIsLoggedIn }) => {
         style={styles.input}
       />
       <CustomButton
-                text={strings.login.loginButton}
-                onPress={handleLogin}
-            />
+        text={strings.login.loginButton}
+        onPress={handleLogin}
+      />
       <Pressable onPress={() => navigation.navigate('Registration')}>
         <View style={styles.signupContainer}>
           <Text style={styles.signupText}>{strings.login.noAccount}</Text>
           <Text style={[styles.signupText, styles.signupLink]}>{strings.login.signupLink}</Text>
         </View>
       </Pressable>
+      <Toast ref={toastRef} />
     </View>
   );
 };
